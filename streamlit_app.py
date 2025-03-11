8c463e2561fbf8883be36edefa9fecded8aa1718
Erna Karsono <e_karsono@hotmail.com>
2025-03-11 18:20:42 +0000

    try json

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,8 +58,8 @@ if ingredients_list:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-st_df = st.text(smoothiefroot_response.json(),use_container_width=True)
+st.text(smoothiefroot_response.json())
+#st_df = st.text(smoothiefroot_response.json(),use_container_width=True)
 
         
    
